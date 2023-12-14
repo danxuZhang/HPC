25f9ef8a1b2382be1ad26ee1419f40b41b372668
dxZhang <dxzhang49@gmail.com>
2023-12-14 09:53:05 +0800

    Format

--- openmp/02-calc-pi/omp.c
+++ openmp/02-calc-pi/omp.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 static const long num_steps = 1000000;
-static const double step = 1.0 / (double) num_steps;
+static const double step = 1.0 / (double)num_steps;
 
 double calculate_pi() {
-    double sum = 0.0;
-    
-#pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < num_steps; ++i) {
-        double x = (i + 0.5) * step;
-        sum += 4.0 / (1.0 + x * x);
-    }
-    
-    return step * sum;
+  double sum = 0.0;
+
+#pragma omp parallel for reduction(+ : sum)
+  for (int i = 0; i < num_steps; ++i) {
+    double x = (i + 0.5) * step;
+    sum += 4.0 / (1.0 + x * x);
+  }
+
+  return step * sum;
 }
 
 int main() {
